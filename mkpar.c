3ae4ecbf000853f72bdd9a04e7d358938eb99b2a
Markus Elfring <elfring@users.sourceforge.net>
2011-02-09 19:55:34 +0100

    Deletion of duplicated function declarations
    Four function declarations were deleted because they were duplicated.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- mkpar.c
+++ mkpar.c
@@ -14,12 +14,6 @@ Yshort final_state;
 static int SRcount;
 static int RRcount;
 
-action *parse_actions(int stateno);
-action *get_shifts(int stateno);
-action *add_reductions(int stateno, action *actions);
-action *add_reduce(action *actions, int ruleno, int symbol);
-
-
 void make_parser()
 {
     register int i;
